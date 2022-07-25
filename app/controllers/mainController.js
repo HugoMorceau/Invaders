3e4685e08a648f46e50992a93933c3d96e11759b
hugo morceau <27mhugo@gmail.com>
2022-07-25 11:19:51 +0200

    tests serv

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -8,6 +8,11 @@ const mainController = {
       // Rendu de la page d'accueil
       console.log(pool)
       console.log('render de l index par le main controller')
+      // test logs ip
+      console.log('logs ip debut ')
+       console.log(req.headers['x-forwarded-for'])
+       console.log(req.socket.remoteAddress )
+       console.log('logs ip fin ')
       res.render('index');
     }
   };
